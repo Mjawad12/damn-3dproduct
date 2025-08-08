35545ec06e573514d49fe491edbe8a4490492e14
Mjawad12 <143606319+Mjawad12@users.noreply.github.com>
2025-08-09 01:50:10 +0500

    server chnages

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = 5500;
 
 app.use(express.json());
-app.use(express.static("damn"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.sendFile("Damn - Digital Hub.html", { root: "damn" });
+  res.sendFile(path.join(__dirname, "public", "Damn - Digital Hub.html"));
 });
 
 app.listen(PORT, () => {
